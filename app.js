c6d7d1dba1bedcb5b126715cce75651359d1ba9c
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-05-11 10:03:05 -0700

    Made a lot of changes. Removed functions and simplified code and questsions.

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-'use strict';
+'use strict'
 
-function greetings () {
-    let userGreeting = prompt ("Hello, what's your favorite color?");
-    let color = prompt("Whoa, " + userGreeting + " is a great color. What do you think my favorite color is?");
-    let guessColor = prompt("I love the color " + color + ", but let's try again!");
-    if (guessColor.toLowerCase() == "black") {
-      guessColor = prompt("Almost, lets give it another go.");
-    } 
-    while (guessColor.toLowerCase() != "yellow") {
-      guessColor = prompt("How about I give you a hint, it's bright.)");
+alert('Hello. It is time to start guessing.');
+
+let guessOne = prompt('Do you know how to play an instrument?').toLocaleLowerCase();
+if(guessOne === 'yes' || guessOne === 'y') {
+    alert('That is right. I do!');
+    } else if(guessOne === 'no' || guessOne === 'n') {
+        alert('Actually, I do.');
     }
-    return guessColor
-}
 
-function colorSelect (colorGuess) {
-    if (colorGuess.toLowerCase() == "yellow") {
-        document.write("<p id='scotties-band'>" + "Amazing. Yellow is it, but take a look at THIS" + "</p>");
-    } else if (colorGuess.toLowerCase() == "black") {
-        document.write("<p id='scotties-band'>" + "Now that's the other one. I love that one too. " + "</p>");
-    } else {
-        document.write("<p id='scotties-band'>" + "Maybe try another color?" + "</p>");
+    let guessTwo = prompt('Do you play video games?').toLocaleLowerCase();
+    if(guessTwo === 'yes' || guessTwo === 'y') {
+        alert('That is also right! I play em all day every day.');
+    } else if(guessTwo === 'no' || guessTwo === 'n') {
+        alert('Wait, you think I dont play? That is not correct.');
     }
-    return colorGuess
-}
-let colorGuess = greetings ()
-
-colorSelect (colorGuess)
-
-let colorYellow = prompt("Absolute correct. Now give me a number one to ten.");
-
-for (let i = 0; i < colorYellow; i++) {
-    document.write("<img id='freddie' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png' />")
-}
